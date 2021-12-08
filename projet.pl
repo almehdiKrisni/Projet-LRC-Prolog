3cad3cf5f381c4c6ef5957b2006d60a3d8f6be2d
Almehdi KRISNI <almehdi.krisni@gmail.com>
2021-12-08 18:16:46 +0100

    Update

--- projet.pl
+++ projet.pl
@@ -1,5 +1,9 @@
-% 
+%                                                            PROJET
+%                                                         LRC - M1 DAC
+%                                                  KRISNI Almehdi - JDAY Achraf
+%                                           https://github.com/krisninho2000/Projet_LRC
 
+% Partie TBox
 equiv(sculpteur,and(personne,some(aCree,sculpture))).
 equiv(auteur,and(personne,some(aEcrit,livre))).
 equiv(editeur,and(personne,and(not(some(aEcrit,livre)),some(aEdite,livre)))).
@@ -28,12 +32,26 @@ rname(aEcrit).
 rname(aEdite).
 rname(aEnfant).
 
+% Partie ABox
+% ABox d'instances
 inst(michelAnge,personne).
 inst(david,sculpture).
 inst(sonnets,livre).
 inst(vinci,personne).
 inst(joconde,objet).
 
+% ABox de relations
 instR(michelAnge, david, aCree).
 instR(michelAnge, sonnets, aEcrit).
 instR(vinci, joconde, aCree).
+
+% Partie 1
+% Cette méthode permet de créer des listes allant contenir la TBox, la ABox d'instances et la ABox de rôles.
+% Ces listes évolueront au fur et à mesure qu'on soumettra des propositions à la démonstration.
+premiere_etape(Tbox, Abi, Abr) :-
+    setof((X, Y), equiv(X, Y), Tbox),
+    setof((X, Y), inst(X, Y), Abi),
+    setof((X, Y, Z), instR(X, Y, Z), Abr).
+
+% Partie 2
+
