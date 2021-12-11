bffb139a48ee8cce3c415c03872adb8a25814c76
unknown <almehdi.krisni@gmail.com>
2021-12-11 17:04:15 +0100

    updates

--- projet.pl
+++ projet.pl
@@ -84,17 +84,70 @@ concept(C) :- cnamena(C), !.
 
 % ------------------------------------------------------------------------------------------
 
-% Règles de remplacement (replace)
+% Prédicat prédéfini testant l'appartenance d'un élément X à une liste L
+member(X, L).
 
 % ------------------------------------------------------------------------------------------
 
-% Règles de concatenation d'éléments (concat)
+% Règles de remplacement (replace)
+replace(and(C1,C2),and(RC1,RC2)) :- replace(C1,RC1), replace(C2,RC2), !.
+replace(or(C1,C2),or(RC1,RC2)) :- replace(C1,RC1), replace(C2,RC2), !.
+replace(all(R,C),all(R,RC)) :- replace(C,RC), !.
+replace(some(R,C),some(R,RC)) :- replace(C,RC), !.
+replace(not(C),not(RC)) :- replace(C,RC), !.
+replace(C,C) :- setof(X,cnamea(X),L), member(C,L), !.
+replace(C,RC) :- setof(X,cnamena(X),L), member(C,L), equiv(C,RC), !.
+
+% ------------------------------------------------------------------------------------------
+
+% Méthode réalise la concaténation de deux listes L1 et L2 et renvoie la liste L3
 concate([],L1,L1).
 concate([X|Y],L1,[X|L2]) :- concat(Y,L1,L2).
+
+% ------------------------------------------------------------------------------------------
+
+% Méthode réalisant la suppression de X de la liste L1 et renvoie la liste résultante L2
+enleve(X,[X|L],L) :- !.
+enleve(X,[Y|L],[Y|L2]) :- enleve(X,L,L2).
+
+% ------------------------------------------------------------------------------------------
+
+% Méthode de génération d'un nouvel identificateur qui est fourni en sortie dans Nom
+genere(Nom) :-  compteur(V), nombre(V,L1),
+                concat([105,110,115,116],L1,L2),
+                V1 is V+1,
+                dynamic(compteur/1),
+                retract(compteur(V)),
+                dynamic(compteur/1),
+                assert(compteur(V1)), nl, nl, nl,
+                name(Nom,L2).
+
+nombre(0,[]).
+nombre(X,L1) :- R is (X mod 10),
+                Q is ((X-R)//10),
+                chiffre_car(R,R1),
+                char_code(R1,R2),
+                nombre(Q,L),
+                concat(L,[R2],L1).
+
+chiffre_car(0,'0').
+chiffre_car(1,'1').
+chiffre_car(2,'2').
+chiffre_car(3,'3').
+chiffre_car(4,'4').
+chiffre_car(5,'5').
+chiffre_car(6,'6').
+chiffre_car(7,'7').
+chiffre_car(8,'8').
+chiffre_car(9,'9').
+
+compteur(1).
 
 % ##########################################################################################
 
 % PARTIE 1
+
+% ##########################################################################################
 
 % Cette méthode permet de créer des listes allant contenir la TBox, la ABox d'instances et la ABox de rôles.
 % Ces listes évolueront au fur et à mesure qu'on soumettra des propositions à la démonstration.
@@ -106,6 +159,14 @@ premiere_etape(Tbox, Abi, Abr) :-
 % ##########################################################################################
 
 % PARTIE 2
+
+% ##########################################################################################
+
+% Méthode de la deuxième étape
+deuxieme_etape(Abi,Abi1,Tbox) :-
+    saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox).
+
+% ------------------------------------------------------------------------------------------
 
 % Mise en place du code fourni par l'énoncé
 saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox) :-
@@ -170,9 +231,20 @@ acquisition_prop_type2(Abi,Abi1,Tbox) :-
     nl, read(C2),
 
     % On effectue les manipulations sur les concepts
-    % On formalise les concepts (C1 et C2), on effectue le remplacement (RC1 et RC2)
+    % On formalise les concepts (C1 et C2) puis on effectue le remplacement (RC1 et RC2)
     concept(C1), concept(C2),
-    replace(and(C1, C2), and(RC1, RC2)),
+    replace(and(C1, C2), and(RC1, RC2)).
+
+    % On génère une instance et on ajoute l'élément (I : C1 and C2) à la ABox
     
 
 % ##########################################################################################
+
+% ##########################################################################################
+
+% MAIN
+
+% ##########################################################################################
+
+exec :- premiere_etape(TBox, Abi, Abr),
+        deuxieme_etape(Abi, Abi1, Tbox).
