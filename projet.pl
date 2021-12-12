ffa0a088d11a4033a06a7ded088cbb7e1d4dfb3b
unknown <almehdi.krisni@gmail.com>
2021-12-12 19:04:55 +0100

    update

--- projet.pl
+++ projet.pl
@@ -5,12 +5,141 @@
 
 % ##########################################################################################
 
+% PARTIE 1
+
+% ##########################################################################################
+
+% Cette méthode permet de créer des listes allant contenir la TBox, la ABox d'instances et la ABox de rôles.
+% Ces listes évolueront au fur et à mesure qu'on soumettra des propositions à la démonstration.
+premiere_etape(Tbox, Abi, Abr) :-
+    setof((X, Y), equiv(X, Y), Tbox),
+    setof((X, Y), inst(X, Y), Abi),
+    setof((X, Y, Z), instR(X, Y, Z), Abr).
+
+% ##########################################################################################
+
+% PARTIE 2
+
+% ##########################################################################################
+
+% Méthode de la deuxième étape
+deuxieme_etape(Abi,Abi1,Tbox) :-
+    saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox).
+
+% ------------------------------------------------------------------------------------------
+
+% Mise en place du code fourni par l'énoncé
+saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox) :-
+    nl,write('Entrer le numero du type de proposition que l"on souhaite demontrer :'),
+    nl,write('\tType 1 - Une instance donnee appartient a un concept donne.'),
+    nl,write('\tType 2 - Deux concepts nelements en commun dont l"intersection est vide (negation).'),
+    nl,read(R),
+    suite(R,Abi,Abi1,Tbox), !.
+
+% ------------------------------------------------------------------------------------------
+
+suite(1,Abi,Abi1,Tbox) :-
+    acquisition_prop_type1(Abi,Abi1,Tbox), !.
+
+% ------------------------------------------------------------------------------------------
+
+suite(2,Abi,Abi1,Tbox) :-
+    acquisition_prop_type2(Abi,Abi1,Tbox), !.
+
+% ------------------------------------------------------------------------------------------
+
+suite(R,Abi,Abi1,Tbox) :-
+    nl,write('Cette option n"existe pas :'),write(R),
+    nl, saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox).
+
+% ------------------------------------------------------------------------------------------
+% On met en place les méthodes utilisées plus haut
+% ------------------------------------------------------------------------------------------
+
+% Méthode permettant l'acquisition de propositions du type 1 (I : C)
+acquisition_prop_type1(Abi,Abi1,Tbox) :-
+    % On entre le type de l'instance
+    nl, write('Veuillez entrer l"instance :'),
+    nl, read(I),
+
+    % On réalise une vérification sur l'instance
+    verificationInstance(I),
+
+    % On entre le concept
+    nl, write('Veuillez entrer le concept :'),
+    nl, read(C),
+
+    % On effectue une vérification sur le concept
+    verificationConcept(C),
+
+    % On effectue les manipulations sur le concept
+    % On formalise le concept (C), on le remplace (RC) puis on effectue sa négation (NRC)
+    replace(C, RC),
+    nnf(not(RC),NRC),
+
+    % On ajoute l'élément (I, NRC) à la ABox
+    concat([((I,NRC))],Abi,Abi1).
+
+% ------------------------------------------------------------------------------------------
+
+% Méthode permettant l'acquisition de proposition du type 2 : (C1 and C2 compris dans 'neg')
+acquisition_prop_type2(Abi,Abi1,Tbox) :-
+    % On entre le concept 1
+    nl, write('Veuillez entrer le concept 1 :'),
+    nl, read(C1),
+
+    % On effectue une vérification sur le concept 1
+    verificationConcept(C1),
+
+    % On entre le concept 2
+    nl, write('Veuillez entrer le concept 2 :'),
+    nl, read(C2),
+
+    % On effectue une vérification sur le concept 2
+    verificationConcept(C2),
+
+    % On effectue les manipulations sur les concepts
+    % On formalise les concepts (C1 et C2) puis on effectue le remplacement (RC1 et RC2)
+    concept(C1), concept(C2),
+    replace(and(C1, C2), and(RC1, RC2)),
+
+    % On génère une instance et on ajoute l'élément (I : C1 and C2) à la ABox
+    genere(Inst),
+    concat([((Inst,and(RC1,RC2)))], Abi, Abi1).
+
+% ##########################################################################################
+
+% PARTIE 3
+
+% ##########################################################################################
+
+% Prédicat de la troisième étape
+troisieme_etape(Abi,Abr) :- 
+    tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls),
+    resolution(Lie,Lpt,Li,Lu,Ls,Abr),
+    nl,write('Youpiiiiii, on a demontre la proposition initiale !!!').
+
+% ##########################################################################################
+
+% MAIN
+
+% ##########################################################################################
+
+programme :- 
+    premiere_etape(TBox, Abi, Abr),
+    deuxieme_etape(Abi, Abi1, Tbox),
+    troisieme_etape(Abi1, Abr).
+
+
+% ##########################################################################################
+
 % PARTIE PREPARATION
 
 % ##########################################################################################
 
 % TBox
-equiv(sculpteur,and(personne,some(aCree,sculpture))).
+equiv(sculpteur,and(personne,some(aCree,sculpture))). 
+% Le prédicat ci-dessus signifie qu'un sculpteur est équivalent à une personne ayant crée une sculpture
 equiv(auteur,and(personne,some(aEcrit,livre))).
 equiv(editeur,and(personne,and(not(some(aEcrit,livre)),some(aEdite,livre)))).
 equiv(parent,and(personne,some(aEnfant,anything))).
@@ -90,29 +219,29 @@ concept(C) :- cnamena(C), !.
 % ------------------------------------------------------------------------------------------
 
 % Règles de remplacement (replace)
-replace(and(C1,C2),and(RC1,RC2)) :- replace(C1,RC1), replace(C2,RC2), !.
-replace(or(C1,C2),or(RC1,RC2)) :- replace(C1,RC1), replace(C2,RC2), !.
-replace(all(R,C),all(R,RC)) :- replace(C,RC), !.
-replace(some(R,C),some(R,RC)) :- replace(C,RC), !.
-replace(not(C),not(RC)) :- replace(C,RC), !.
-replace(C,C) :- setof(X,cnamea(X),L), member(C,L), !.
-replace(C,RC) :- setof(X,cnamena(X),L), member(C,L), equiv(C,RC), !.
+replace(C,C) :- cnamea(C).
+replace(not(C),not(RC)) :- replace(C,RC).
+replace(and(C1,C2),and(RC1,RC2)) :- replace(C1,RC1), replace(C2,RC2).
+replace(or(C1,C2),or(RC1,RC2)) :- replace(C1,RC1), replace(C2,RC2).
+replace(all(R,C),all(R,RC)) :- replace(C,RC).
+replace(some(R,C),some(R,RC)) :- replace(C,RC).
+replace(C,RC) :- equiv(C,C2), replace(C2,RC).
 
 % ------------------------------------------------------------------------------------------
 
-% Méthode réalise la concaténation de deux listes L1 et L2 et renvoie la liste L3
+% Prédicat réalisant la concaténation de deux listes L1 et L2 et renvoie la liste L3
 concat([],L1,L1).
 concat([X|Y],L1,[X|L2]) :- concat(Y,L1,L2).
 
 % ------------------------------------------------------------------------------------------
 
-% Méthode réalisant la suppression de X de la liste L1 et renvoie la liste résultante L2
+% Prédicat réalisant la suppression de X de la liste L1 et renvoie la liste résultante L2
 enleve(X,[X|L],L) :- !.
 enleve(X,[Y|L],[Y|L2]) :- enleve(X,L,L2).
 
 % ------------------------------------------------------------------------------------------
 
-% Méthode de génération d'un nouvel identificateur qui est fourni en sortie dans Nom
+% Prédicat de génération d'un nouvel identificateur qui est fourni en sortie dans Nom
 genere(Nom) :-  compteur(V), nombre(V,L1),
                 concat([105,110,115,116],L1,L2),
                 V1 is V+1,
@@ -143,120 +272,148 @@ chiffre_car(9,'9').
 
 compteur(1).
 
-% ##########################################################################################
+% ------------------------------------------------------------------------------------------
 
-% PARTIE 1
-
-% ##########################################################################################
-
-% Cette méthode permet de créer des listes allant contenir la TBox, la ABox d'instances et la ABox de rôles.
-% Ces listes évolueront au fur et à mesure qu'on soumettra des propositions à la démonstration.
-premiere_etape(Tbox, Abi, Abr) :-
-    setof((X, Y), equiv(X, Y), Tbox),
-    setof((X, Y), inst(X, Y), Abi),
-    setof((X, Y, Z), instR(X, Y, Z), Abr).
-
-% ##########################################################################################
-
-% PARTIE 2
-
-% ##########################################################################################
-
-% Méthode de la deuxième étape
-deuxieme_etape(Abi,Abi1,Tbox) :-
-    saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox).
+% Prédicat de vérification sur les instances
+verificationInstance(I) :-
+    iname(I), !.
 
 % ------------------------------------------------------------------------------------------
 
-% Mise en place du code fourni par l'énoncé
-saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox) :-
-    nl,write('Entrer le numéro du type de proposition que l"on souhaite démontrer:'),
-    nl,write('1 Une instance donnée appartient à un concept donne.'),
-    nl,write('2 Deux concepts nelements en commun(ils ont uneintersection vide).'),
-    nl,read(R),
-    suite(R,Abi,Abi1,Tbox), !.
+% Prédicat de vérification sur les concepts
+% Les concepts peuvent être de différentes formes donc le prédicat a plusieurs formes
+verificationConcept(C) :-
+    cnamea(C), !.
+
+verificationConcept(C) :- 
+    cnamena(C), !.
+
+verificationConcept(not(C)) :- 
+    verificationConcept(C), !.
+
+verificationConcept(or(C1, C2)) :- 
+    verificationConcept(C1), 
+    verificationConcept(C2), !.
+
+verificationConcept(and(C1, C2)) :- 
+    verificationConcept(C1), 
+    verificationConcept(C2), !.
+
+verificationConcept(some(R, C)) :- 
+    rname(R), 
+    verificationConcept(C), !.
+
+verificationConcept(all(R, C)) :- 
+    rname(R), 
+    verificationConcept(C), !.
 
 % ------------------------------------------------------------------------------------------
 
-suite(1,Abi,Abi1,Tbox) :-
-    acquisition_prop_type1(Abi,Abi1,Tbox), !.
+% Prédicat de tri des différentes box
+tri_Abox([],[],[],[],[],[]).
+
+tri_Abox([(I,some(R,C))|Abi],[(I,some(R,C))|Lie],Lpt,Li,Lu,Ls) :- 
+  tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls).
+
+tri_Abox([(I,all(R,C))|Abi],Lie,[(I,all(R,C))|Lpt],Li,Lu,Ls) :- 
+  tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls).
+
+tri_Abox([(I,and(C1,C2))|Abi],Lie,Lpt,[(I,and(C1,C2))|Li],Lu,Ls) :- 
+  tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls).
+
+tri_Abox([(I,or(C1,C2))|Abi],Lie,Lpt,Li,[(I,or(C1,C2))|Lu],Ls) :- 
+  tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls).
+
+tri_Abox([E|Abi],Lie,Lpt,Li,Lu,[E|Ls]) :- 
+  tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls).
 
 % ------------------------------------------------------------------------------------------
 
-suite(2,Abi,Abi1,Tbox) :-
-    acquisition_prop_type2(Abi,Abi1,Tbox), !.
+% Prédicat réalisant la résolution de propositions
+resolution([],[],[],[],Ls,Abr) :- 
+    not(verificationClash(Ls)), write("Feuille Ouverte"), nl, !.
+
+resolution(Lie,Lpt,Li,Lu,Ls,Abr) :-
+	verificationClash(Ls),
+	Lie \== [],
+	complete_some(Lie,Lpt,Li,Lu,Ls,Abr).
+
+resolution(Lie,Lpt,Li,Lu,Ls,Abr) :-
+	verificationClash(Ls),
+	Li \== [],
+	transformation_and(Lie,Lpt,Li,Lu,Ls,Abr).
+
+resolution(Lie,Lpt,Li,Lu,Ls,Abr) :-
+	verificationClash(Ls),
+	Lpt \==[],
+	deduction_all(Lie,Lpt,Li,Lu,Ls,Abr).
+
+resolution(Lie,Lpt,Li,Lu,Ls,Abr) :-
+	verificationClash(Ls),
+	Lu \==[],
+	transformation_or(Lie,Lpt,Li,Lu,Ls,Abr).
 
 % ------------------------------------------------------------------------------------------
 
-suite(R,Abi,Abi1,Tbox) :-
-    nl,write('Cette option n"existe pas : '),write(R),
-    nl, saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox).
+% Prédicat réalisant la vérification de problèmes lors de la résolution de proposition
+verificationClash([(I,C)|Ls]) :-
+    nnf(not(C),NC),
+    member((I,NC), Ls).
 
-% ------------------------------------------------------------------------------------------
-% On met en place les méthodes utilisées plus haut
-% ------------------------------------------------------------------------------------------
-
-% Méthode permettant l'acquisition de propositions du type 1 (I : C)
-acquisition_prop_type1(Abi,Abi1,Tbox) :-
-    % On entre le type de l'instance
-    nl, write('Veuillez entrer l"instance :'),
-    nl, read(I),
-
-    % On récupère la liste des noms d'instances et on vérifie l'appartenance de I à L
-    setof(X, iname(X), L),
-    member(I,L),
-
-    % On entre le concept
-    nl, write('Veuillez entrer le concept :'),
-    nl, read(C),
-
-    % On effectue les manipulations sur le concept
-    % On formalise le concept (C), on le remplace (RC) puis on effectue sa négation (NRC)
-    concept(C),
-    replace(C, RC),
-    nnf(not(RC),NRC),
-
-    % On ajoute l'élément (I, NRC) à la ABox
-    concat([((I,NRC))],Abi,Abi1),
-
-    % On affiche la ABox après modification
-    nl, write(Abi1).
+verificationClash([_|Ls]) :- 
+    verificationClash(Ls).
 
 % ------------------------------------------------------------------------------------------
 
-% Méthode permettant l'acquisition de proposition du type 2 : (C1 and C2 compris dans 'neg')
-acquisition_prop_type2(Abi,Abi1,Tbox) :-
-    % On entre le concept 1
-    nl, write('Veuillez entrer le concept 1 :'),
-    nl, read(C1),
+% Prédicat d'évolution
+% Permet la mise à jour des différentes listes lors de la résolution
+evolue((I,and(A,B)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, [(I,and(A,B))|Li], Lu, Ls).
+evolue((I,all(R,C)), Lie, Lpt, Li, Lu, Ls, Lie, [(I,all(R,C))|Lpt], Li, Lu, Ls).
+evolue((I,some(R,C)), Lie, Lpt, Li, Lu, Ls, [(I,some(R,C))|Lie], Lpt, Li, Lu, Ls).
+evolue((I,or(C1,C2)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, [(I,or(C1,C2))|Lu], Ls).
+evolue(Elem, Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, [(Elem)|Ls]).
 
-    % On entre le concept 2
-    nl, write('Veuillez entrer le concept 2 :'),
-    nl, read(C2),
+% ------------------------------------------------------------------------------------------
 
-    % On effectue les manipulations sur les concepts
-    % On formalise les concepts (C1 et C2) puis on effectue le remplacement (RC1 et RC2)
-    concept(C1), concept(C2),
-    replace(and(C1, C2), and(RC1, RC2)),
+% Prédicat de complétion
+% Représente la règle : 'il existe'
+complete_some([],Lpt,Li,Lu,Ls,Abr):-
+    transformation_and([],Lpt,Li,Lu,Ls,Abr).
 
-    % On génère une instance et on ajoute l'élément (I : C1 and C2) à la ABox
-    genere(Inst),
-    concat([((Inst,and(RC1,RC2)))], Abi, Abi1),
+complete_some([(I1,some(R,C))|Lie],Lpt,Li,Lu,Ls,Abr) :-
+    genere(I2),
+    Abr1 = Abr,
+    evolue((I,C),Lie,Lpt,Li,Lu,Ls,Lie1,Lpt1,Li1,Lu1,Ls1),
+    Abr2 = [(I1,I2,R)|Abr],
+    resolution(Lie1,Lpt1,Li1,Lu1,Ls1,[(I1,I2,R)|Abr]).
 
-    % On affiche la ABox après modification
-    nl, write(Abi1).
+% ------------------------------------------------------------------------------------------
 
-% ##########################################################################################
+% Prédicat de déduction
+% Représente la règle : 'pour tout'
+deduction_all(Lie,[],Li,Lu,Ls,Abr) :-
+    transformation_or(Lie,[],Li,Lu,Ls,Abr).
 
-% PARTIE 3
+deduction_all(Lie,[(I,all(R,C))|Lpt],Li,Lu,Ls,Abr) :-
+    member((I,B,R),Abr),
+    evolue((B,C),Lie,Lpt,Li,Lu,Ls,Lie1,Lpt1,Li1,Lu1,Ls1),
+    resolution(Lie1,Lpt1,Li1,Lu1,Ls1,Abr).
 
-% ##########################################################################################
+% ------------------------------------------------------------------------------------------
 
-% ##########################################################################################
+% Prédicat de transformation (formule 'or' et 'and')
+% Représente la règle 'ou'
+transformation_or(Lie,Lpt,Li,[(I,or(C,D))|Lu],Ls,Abr):- 
+    evolue((I,C),Lie,Lpt,Li,Lu,Ls,Lie1,Lpt1,Li1,Lu1,Ls1),
+    evolue((I,D),Lie,Lpt,Li,Lu,Ls,Lie2,Lpt2,Li2,Lu2,Ls2),
+    resolution(Lie1,Lpt1,Li1,Lu1,Ls1,Abr),
+    resolution(Lie2,Lpt2,Li2,Lu2,Ls2,Abr).
 
-% MAIN
+% Représente la règle 'et'
+transformation_and(Lie,Lpt,[],Lu,Ls,Abr) :-
+    deduction_all(Lie,Lpt,[],Lu,Ls,Abr).
 
-% ##########################################################################################
-
-exec :- premiere_etape(TBox, Abi, Abr),
-        deuxieme_etape(Abi, Abi1, Tbox).
+transformation_and(Lie,Lpt,[(I,and(A,B))|Li],Lu,Ls,Abr) :-
+    evolue((I,A),Lie,Lpt,Li,Lu,Ls,Lie1,Lpt1,Li1,Lu1,Ls1),
+    evolue((I,B),Lie1,Lpt1,Li1,Lu1,Ls1,Lie2,Lpt2,Li2,Lu2,Ls2),
+    resolution(Lie2,Lpt2,Li2,Lu2,Ls2,Abr).
